06dde23c052c81c260f8ec5f239b35e1628751ee
Agzes <103037173+Agzes@users.noreply.github.com>
2026-04-26 00:37:11 +1200

    ⚙️ • FluAutoClicker next

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+
+const host = process.env.TAURI_DEV_HOST;
+const devHost = host || "127.0.0.1";
+
+export default defineConfig(async () => ({
+  root: "src",
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: devHost,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: "src/index.html",
+        cps: "src/cps.html",
+      },
+    },
+  },
+}));
